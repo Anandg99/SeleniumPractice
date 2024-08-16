260facfc66ced2bc0d7724bd709a508f4a1e2009
Ananda Babu Gummadilli <Anand.Gummadilli@neudesic.com>
2024-08-16 07:55:12 +0530

    Added new class file clsLogging

---
+++ Selenium_Demo/src/test/java/SeleniumLearning/clsLogging.java
@@ -0,0 +1,12 @@
+package SeleniumLearning;
+import org.testng.annotations.*;
+public class clsLogging {
+	@Test
+	public void TestCase1()
+	{
+	  System.out.println("I am test case1");
+	}
+	
+	
+
+}
